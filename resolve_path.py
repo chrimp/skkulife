5124f9698ff8472d2e7cc2e61194f5c8f95d550c
김태우 <60501603+chrimp@users.noreply.github.com>
2024-11-04 15:36:45 +0900

    rel automation

---
+++ resolve_path.py
@@ -0,0 +1,14 @@
+import os
+
+path = "./templates"
+
+for _, _, files in os.walk(path):
+    for file in files:
+        if file.endswith(".html"):
+            print(file)
+            file_path = os.path.join(path, file)
+            with open(file_path, 'r', encoding="utf-8") as f:
+                content = f.read()
+            content = content.replace('../static', '/html/static')
+            with open(file_path, 'w', encoding="utf-8") as f:
+                f.write(content)
